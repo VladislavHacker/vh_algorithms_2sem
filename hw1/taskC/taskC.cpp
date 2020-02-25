bdac7fa7998f8a0deba0753f63df20cdb8aea0d2
Vladislav <vladislavhacker111@gmail.com>
2020-02-25 15:55:40 +0300

    codestyle fix

--- hw1/taskC/taskC.cpp
+++ hw1/taskC/taskC.cpp
@@ -1,106 +1,146 @@
-/* Троян-Головян Владислав */
-/* Д. З. 1, задача C "Районы, кварталы, жилые массивы" */
-/* Фрэнку опять прилетел новый заказ. Однако в этот раз город
- * играет по очень странным правилам: все дороги в городе односторонние и
- * связывают только офисы нанимателей перевозчика. Множество офисов, между любыми двумя из
- * которых существует путь, образуют квартал, если нельзя добавить никакие другие, чтобы условие выполнялось.
- * Фрэнку интересно, каково минимальное количество односторонних дорог нужно ещё построить, чтобы весь город
- * стал квраталом.  */
+/* Troyan-Golovyan Vladislav */
+/* Contest 1, Task C */
 #include <iostream>
 #include <vector>
 #include <utility>
-/* DFS для получения порядка обхода */
-void dfs(std::vector<std::vector<int>> &graph, int v, std::vector<char> &colors, std::vector<int> &order) {
+#include <functional>
+/* DFS */
+void dfs (
+    const std::vector< std::vector<size_t> > &graph,
+    size_t v,
+    std::vector<char> &colors,
+    const std::function<void (const size_t)> &in_callback,
+    const std::function<void (const size_t)> &out_callback
+) {
     colors[v] = 1;
-    for (int i = 0; i < graph[v].size(); ++i) {
-        if (colors[graph[v][i]] == 0)
-            dfs(graph, graph[v][i], colors, order);
+    in_callback(v);
+    for (size_t i = 0; i < graph[v].size(); ++i) {
+        if (colors[graph[v][i]] == 0) {
+            dfs(graph, graph[v][i], colors, in_callback, out_callback);
+        }
     }
-    colors[v] = 2;
-    order.push_back(v);
-}
-/* DFS для поиска компонент связности */
-void component_dfs(std::vector<std::vector<int>> &graph, int v, std::vector<char> &colors, std::vector<int> &component, std::vector<int> &v_comp, int comp_num) {
-    component.push_back(v);
-    colors[v] = 1;
-    v_comp[v] = comp_num;
-    for (int i = 0; i < graph[v].size(); ++i) {
-        if (colors[graph[v][i]] == 0)
-            component_dfs(graph, graph[v][i], colors, component, v_comp, comp_num);
-    }
+    out_callback(v);
     colors[v] = 2;
 }
-/* Конденсация на основании данных полученым с помощью алгоритма Косарайю */
-void condense_graph(std::vector<std::vector<int>> &graph, std::vector<std::vector<int>> &condensed_graph, std::vector<int> v_comp) {
-    for (int i = 0; i < graph.size(); ++i)
-        for (int j = 0; j < graph[i].size(); ++j)
-            if (v_comp[i] != v_comp[graph[i][j]])
-                condensed_graph[v_comp[i]].push_back(v_comp[graph[i][j]]);
-}
-/* Функция для получения конденсированного орграфа из исходного */
-void make_condensed_graph(std::vector<std::vector<int>> &graph, std::vector<std::vector<int>> &trans_graph, std::vector<std::vector<int>> &condensed_graph) {
-    std::vector<int> order;
-    std::vector<char> colors(graph.size(), 0);
-
+/* Function for graph condensation */
+void condense_graph (
+    const std::vector< std::vector<size_t> > &graph,
+    std::vector< std::vector<size_t> > &condensed_graph,
+    std::vector<size_t> v_comp
+) {
     for (int i = 0; i < graph.size(); ++i) {
+        for (int j = 0; j < graph[i].size(); ++j) {
+            if (v_comp[i] != v_comp[graph[i][j]]) {
+                condensed_graph[v_comp[i]].push_back(v_comp[graph[i][j]]);
+            }
+        }
+    }
+}
+/* Function for getting condensed oriented graph from */
+void make_condensed_graph (
+    const std::vector<std::vector<size_t>> &graph,
+    const std::vector<std::vector<size_t>> &trans_graph,
+    std::vector<std::vector<size_t>> &condensed_graph
+) {
+    std::vector<size_t> order;
+    std::vector<char> colors(graph.size(), 0);
+    for (size_t i = 0; i < graph.size(); ++i) {
         if (colors[i] == 0) {
-            dfs(graph, i, colors, order);
+            dfs(
+                 graph, i, colors,
+                 [&] (const size_t v) { },
+                 [&] (const size_t v) {
+                     order.push_back(v);
+                 }
+            );
         }
     }
 
-    colors.assign(graph.size(), 0);
-    std::vector<std::vector<int>> components;
-    std::vector<int> v_comp(graph.size(), 0);
+    colors.assign(trans_graph.size(), 0);
+    std::vector<std::vector<size_t>> components;
+    std::vector<size_t> v_comp(graph.size(), 0);
 
-    for (int i = graph.size() - 1; i >= 0; --i) {
-        if (colors[order[i]] == 0) {
-            std::vector<int> component;
-            component_dfs(trans_graph, order[i], colors, component, v_comp, components.size());
+    for (size_t i = 0; i < trans_graph.size(); ++i) {
+        if (colors[order[graph.size() - i  - 1]] == 0) {
+            std::vector<size_t> component;
+            dfs(
+                trans_graph,
+                order[graph.size() - i  - 1],
+                colors,
+                [&] (const size_t v) {
+                    component.push_back(v);
+                    v_comp[v] = components.size();
+                },
+                [&] (const size_t v) {}
+            );
             components.push_back(component);
         }
     }
-
     condensed_graph.clear();
-    condensed_graph.assign(components.size(), std::vector<int> ());
+    condensed_graph.assign(components.size(), std::vector<size_t> ());
     condense_graph(graph, condensed_graph, v_comp);
 }
-/* Поиск min количества дорог, необходимых чтобы дополнить ациклический орграф до сильной связности */
-int count_roads(std::vector<std::vector<int>> &graph,  std::vector<std::pair<int, int>> &in_out) {
+/* Finds min edges count we need to build to make your graph strongly connected  */
+size_t count_roads (
+    const std::vector< std::vector<size_t> > &graph,
+    std::vector< std::pair<size_t, size_t> > &in_out
+) {
     if (graph.size() == 1)
         return 0;
-    for (int i = 0; i < graph.size(); ++i) {
+    for (size_t i = 0; i < graph.size(); ++i) {
         in_out[i].first += graph[i].size();
-        for (int j = 0; j < graph[i].size(); ++j) {
-            in_out[graph[i][j]].second++;
+        for (size_t j : graph[i]) {
+            in_out[j].second++;
         }
     }
-    int count1 = 0;
-    int count2 = 0;
-    for (int i = 0; i < in_out.size(); ++i) {
-        if (in_out[i].first == 0)
+    size_t count1 = 0;
+    size_t count2 = 0;
+    for (auto & deg : in_out) {
+        if (deg.first == 0) {
             count1++;
-        if (in_out[i].second == 0)
+        }
+        if (deg.second == 0) {
             count2++;
+        }
     }
     return count1 < count2 ? count2 : count1;
 }
-
-int main() {
-    int n, m;
+/* Get input data */
+void get_input (
+    size_t &n,
+    size_t &m,
+    std::vector< std::vector<size_t> > &graph,
+    std::vector< std::vector<size_t> > &trans_graph
+) {
     std::cin >> n >> m;
-    std::vector<std::vector<int>> graph(n);
-    std::vector<std::vector<int>> trans_graph(n);
-    for (int i = 0; i < m; ++i) {
-        int a, b;
+    graph.assign(n, std::vector<size_t> ());
+    trans_graph.assign(n, std::vector<size_t> ());
+    for (size_t i = 0; i < m; ++i) {
+        size_t a, b;
         std::cin >> a >> b;
-        a--;
-        b--;
-        graph[a].push_back(b);
-        trans_graph[b].push_back(a);
+        graph[a - 1].push_back(b - 1);
+        trans_graph[b - 1].push_back(a - 1);
     }
-    std::vector<std::vector<int>> condensed_graph;
+}
+/* Set output data */
+void set_output (int count) {
+    std::cout << count;
+}
+
+int main () {
+
+    size_t n, m;
+    std::vector< std::vector<size_t> > graph;
+    std::vector< std::vector<size_t> > trans_graph;
+
+    get_input(n, m, graph, trans_graph);
+
+    std::vector< std::vector<size_t> > condensed_graph;
     make_condensed_graph(graph, trans_graph, condensed_graph);
-    std::vector<std::pair<int, int>> in_out_counts(condensed_graph.size(), std::pair<int, int>(0, 0));
-    std::cout << count_roads(condensed_graph, in_out_counts);
+    std::vector< std::pair<size_t, size_t> > in_out_counts(condensed_graph.size(), std::pair<size_t, size_t>(0, 0));
+
+    set_output(count_roads(condensed_graph, in_out_counts));
+
     return 0;
+
 }
