d8c56e167234de2fe5271564e663c7acf6f76c23
Vladislav <vladislavhacker111@gmail.com>
2020-02-25 16:07:19 +0300

    codestyle fix task A

--- hw_1/task_A/task_A.cpp
+++ hw_1/task_A/task_A.cpp
@@ -1,55 +1,74 @@
-/* Троян-Головян Владислав */
-/* Д. З. 1, задача "За молоком" */
-/* Леон и Матильда собрались пойти в магазин за молоком. Их хочет поймать Стэнсфилд, поэтому нашим товарищам нужно
- * сделать это как можно быстрее. Каково минимальное количество улиц, по которым пройдёт хотя бы один из ребят (либо
- * Матильда, либо Леон, либо оба вместе)?  */
-
+/* Troyan-Golovyan Vladislav */
+/* Contest 1, Task A */
 #include <iostream>
 #include <vector>
 #include <queue>
-/* Поиск в ширину */
-void BFS(const std::vector <std::vector <int> > &graph, const int v, const int f, std::vector<int> &ways) {
-    std::queue<int> q;
+/* BFS */
+void bfs (const std::vector <std::vector <size_t> > &graph, const size_t &v, const size_t &f, std::vector<size_t> &ways) {
+    std::queue<size_t> q;
     q.push(v);
     ways[v] = 0;
     while (!q.empty()) {
         int current = q.front();
         q.pop();
-        for (auto it = graph[current].begin(); it != graph[current].end(); ++it) {
-            if (ways[*it] == -1) {
-                q.push(*it);
-                ways[*it] = ways[current] + 1;
+        for (size_t u : graph[current]) {
+            if (ways[u] == -1) {
+                q.push(u);
+                ways[u] = ways[current] + 1;
             }
         }
     }
 }
-/* Поиск количества дорог */
-unsigned long long find_way_count(std::vector<int> &matilda_ways, std::vector<int> &leon_ways,  std::vector<int> &milk_ways, int size) {
-    int min = matilda_ways[0] + milk_ways[0] + leon_ways[0];
-    for (int i = 0; i < size; i++) {
+/* Find ways count */
+size_t find_way_count (
+    const std::vector<size_t> &matilda_ways,
+    const std::vector<size_t> &leon_ways,
+    const std::vector<size_t> &milk_ways
+) {
+    size_t min = matilda_ways[0] + milk_ways[0] + leon_ways[0];
+    for (size_t i = 0; i < matilda_ways.size(); i++) {
        if (matilda_ways[i] + milk_ways[i] + leon_ways[i] < min) {
            min = matilda_ways[i] + milk_ways[i] + leon_ways[i];
        }
     }
     return min;
 }
-
-int main() {
-    int n, m, matilda, leon, milk;
+/* Get input */
+void get_input (
+    std::vector <std::vector <size_t> > &graph,
+    size_t &n,
+    size_t &m,
+    size_t &matilda,
+    size_t &leon,
+    size_t &milk
+) {
     std::cin >> n >>  m >> leon >> matilda >> milk;
-    std::vector <std::vector <int> > graph(n);
+    graph.assign(n, std::vector<size_t> ());
     int a, b;
     for (int i = 0; i < m; ++i) {
         std::cin >> a >> b;
         graph[a - 1].push_back(b - 1);
         graph[b - 1].push_back(a - 1);
     }
-    std::vector<int> matilda_ways(graph.size(), -1);
-    BFS(graph, matilda - 1, milk - 1, matilda_ways);
-    std::vector<int> leon_ways(graph.size(), -1);
-    BFS(graph, leon - 1, milk - 1, leon_ways);
-    std::vector<int> milk_ways(graph.size(), -1);
-    BFS(graph, milk - 1, milk - 1, milk_ways);
-    std::cout << find_way_count(matilda_ways, leon_ways, milk_ways, graph.size());
+}
+/* Set output */
+void set_output(size_t count) {
+    std::cout << count;
+}
+
+int main() {
+    size_t n, m, matilda, leon, milk;
+    std::vector <std::vector <size_t> > graph;
+    get_input(graph, n, m, matilda, leon, milk);
+
+    std::vector<size_t> matilda_ways(graph.size(), -1);
+    bfs(graph, matilda - 1, milk - 1, matilda_ways);
+    std::vector<size_t> leon_ways(graph.size(), -1);
+    bfs(graph, leon - 1, milk - 1, leon_ways);
+    std::vector<size_t> milk_ways(graph.size(), -1);
+    bfs(graph, milk - 1, milk - 1, milk_ways);
+
+    set_output(find_way_count(matilda_ways, leon_ways, milk_ways));
+
     return 0;
 }
